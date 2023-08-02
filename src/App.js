f78a51fc8e0c602ce097c06ea2b71c2a6f003494
DanielDeda <daniel.costa.deda.oliveira@gmail.com>
2023-08-02 15:21:06 -0300

    refactoring and using map/filter/reduce

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './index.css';
 import Header from './components/header';
 import Footer from './components/footer';
 import Note from './components/note';
+import notes from './notes';
+
+const newNotes = notes.map((note) => note);
+
+console.log(newNotes)
 
 function App() {
   return (<div>
---
+++ src/notes.js
@@ -0,0 +1,20 @@
+const notes = [
+    {
+        key: 1,
+        title: "Delegation",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+    },
+    {
+        key: 2,
+        title: "Blabla",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+    },
+    {
+        key: 3,
+        title: "Bleble",
+        content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."
+    }
+]
+
+
+export default notes;
